1db2da8ab6cbb263b2e37749600ce6e78cab0d9c
muhammad-umer-123 <umer@kodware.net>
2025-10-05 17:13:07 +0500

    not found file added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <h1 className="text-2xl font-bold text-gray-700">
+          404 - Page Not Found
+        </h1>
+      </div>
+    );
+  }
+  
